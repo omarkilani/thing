88b54d392e5db5be8409a26a7ef7d3380d3b82e5
omar <omar@rememberthemilk.com>
2021-06-15 23:41:30 -0700

    Increase max file size, implement thinktime param.

--- main.go
+++ main.go
@@ -11,21 +11,36 @@ import (
 	"time"
 )
 
-const BUF_SIZE = 8 * 1024
-const FILE = "./random_11342319_byte_file"
-const FILE_MAX = 11342319 // save a stat + we want to compare hashes
+const BUF_SIZE = (8 * 1024) + 1 // misalign buffer size
+const FILE = "./random_52428800_byte_file"
+const FILE_DEFAULT = 11342319 // this cauess 3 minutes of stall
+const FILE_MAX = 52428800
 const PAUSE_TIME = 7944 * time.Microsecond
 const THINK_TIME = PAUSE_TIME * 1385
 
-// hacked up https://golang.org/src/io/io.go?s=13817:13895#L422
-var errInvalidWrite = errors.New("invalid write result")
-
-func min(x, y int64) int64 {
+func min(x, y uint64) uint64 {
 	if x < y {
 		return x
 	}
 	return y
 }
+
+func minInt64(x, y int64) int64 {
+	if x < y {
+		return x
+	}
+	return y
+}
+
+func maxInt64(x, y int64) int64 {
+	if x > y {
+		return x
+	}
+	return y
+}
+
+// hacked up https://golang.org/src/io/io.go?s=13817:13895#L422
+var errInvalidWrite = errors.New("invalid write result")
 
 func copyBy(dst io.Writer, src io.Reader, size uint64, withWait bool) (written int64, err error) {
 	buf := make([]byte, size)
@@ -68,13 +83,19 @@ func serve(w http.ResponseWriter, r *http.Request) {
 	case "/think", "/drip":
 		withCopyWait := r.URL.Path == "/drip"
 		if r.URL.Path == "/think" {
-			log.Println("thinking")
-			time.Sleep(THINK_TIME)
+			thinkTime, err := strconv.ParseInt(r.URL.Query().Get("thinktime"), 10, 64)
+			if thinkTime < 0 || err != nil {
+				thinkTime = int64(THINK_TIME)
+			}
+			thinkTime = minInt64(int64(THINK_TIME), int64(time.Duration(thinkTime)*time.Microsecond))
+			log.Printf("thinking for %dns", thinkTime)
+			time.Sleep(time.Duration(thinkTime))
 		}
 
 		w.Header().Set("Content-Type", "text/javascript; charset=utf-8")
 		w.Header().Set("Expires", "Thu, 19 Nov 1981 08:52:00 GMT")
 		w.Header().Set("Cache-Control", "private, max-age=0, no-store, no-cache, must-revalidate")
+
 		f, err := os.Open(FILE)
 		if err != nil {
 			w.WriteHeader(http.StatusInternalServerError)
@@ -83,16 +104,17 @@ func serve(w http.ResponseWriter, r *http.Request) {
 		defer f.Close()
 
 		w.WriteHeader(http.StatusOK)
-		limit, _ := strconv.ParseInt(r.URL.Query().Get("limit"), 10, 64)
+
+		limit, _ := strconv.ParseUint(r.URL.Query().Get("limit"), 10, 64)
 		if limit == 0 {
-			log.Printf("full serve, withCopyWait: %t", withCopyWait)
-			copyBy(w, f, BUF_SIZE, withCopyWait)
+			limit = FILE_DEFAULT
 		} else {
 			limit = min(limit, FILE_MAX)
-			log.Printf("partial serve, withCopyWait: %t, limit = %d", withCopyWait, limit)
-			lr := &io.LimitedReader{R: f, N: limit}
-			copyBy(w, lr, BUF_SIZE, withCopyWait)
 		}
+
+		log.Printf("serve: withCopyWait: %t, limit = %d", withCopyWait, limit)
+		lr := &io.LimitedReader{R: f, N: int64(limit)}
+		copyBy(w, lr, BUF_SIZE, withCopyWait)
 	default:
 		fmt.Fprintf(w, "moo")
 	}
